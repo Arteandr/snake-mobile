2f8daa3103f4ddb366ab51b007350389b571a953
snak1o <snakwcs@gmail.com>
2020-08-23 13:52:52 +0300

    Настроил архитектуру

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AllAccountsComponent from "./app/components/AllAccountsComponent";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Руслан Лысенко.</Text>
-      <StatusBar style="auto" />
+      <AllAccountsComponent />
     </View>
   );
 }
@@ -14,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ app/assets/config/colors.js
@@ -0,0 +1,8 @@
+export default {
+  primary: " #F8777D",
+  secondary: "#65BCBF",
+  black: "#000",
+  white: "#fff",
+  darkgray: "#232C2D",
+  gray: "#EAEBED",
+};
